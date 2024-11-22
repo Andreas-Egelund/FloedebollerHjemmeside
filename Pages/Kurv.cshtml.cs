430998cfb3c9401f8f4f1eebbf1fb6ab29bfe4c6
Andreas-Egelund <ane002@edu.zealand.dk>
2024-11-22 12:23:47 +0100

    Made some sites :)

---
+++ Pages/Kurv.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FloedebollerHjemmeside.Pages
+{
+    public class KurvModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
